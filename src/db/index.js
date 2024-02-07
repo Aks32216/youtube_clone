caf6b99728ee0d674d0d4068daab1930f80c869a
Aks32216 <amishsingh568@gmail.com>
2024-02-07 11:42:37 +0530

    connected to DB

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async ()=>{
+    try {
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        // console.log(connectionInstance);
+        console.log(`\n connected to mongoDB || DB HOST : ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("DB ERROR: ",error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+dotenv.config({
+    path: '../.env'
+})
+
+
+connectDB();    
+
+
+/*
+
+this is one approach to connect to db and require app
+
+import express from "express";
+const app=express();
+import { DB_NAME } from "./constants";
+
+(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        app.on("error", (error)=>{
+            console.log("Application not connected to database");
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log("App is listening to port ",process.env.PORT);
+        })
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+})();
+
+*/
